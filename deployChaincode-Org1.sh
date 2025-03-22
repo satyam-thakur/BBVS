730ff4e16f9029d55c248e6be23bdfe51dc7aa8f
Ubuntu <ubuntu@ip-172-31-89-146.ec2.internal>
2025-03-22 05:15:21 +0000

    updated deployChanincode-peer1

---
+++ deployChaincode-Org1.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+source ./env_variables.sh
+
+# presetup
+
+CHANNEL_NAME="mychannel"
+export CHANNEL_NAME=mychannel
+CC_RUNTIME_LANGUAGE="golang"
+VERSION="1"
+CC_SRC_PATH="./artifacts/src/github.com/Ballot8"
+CC_NAME="voting8"
+
+packageChaincode() {
+    rm -rf ${CC_NAME}.tar.gz
+    setGlobalsForPeer0Org1
+    export GOFLAGS="-buildvcs=false"
+
+    peer lifecycle chaincode package ${CC_NAME}.tar.gz \
+        --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} \
+        --label ${CC_NAME}_${VERSION}
+    echo "===================== Chaincode is packaged on peer0.org1 ===================== "
+}
+# packageChaincode
+
+installChaincode() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode install ${CC_NAME}.tar.gz
+    echo "===================== Chaincode is installed on peer0.org1 ===================== "
+}
+queryInstalled() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode queryinstalled >&log.txt
+    cat log.txt
+    PACKAGE_ID=$(sed -n "/${CC_NAME}_${VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+    echo PackageID is ${PACKAGE_ID}
+    echo "===================== Query installed successful on peer0.org1 on channel ===================== "
+}
+
+# queryInstalled
+
+approveForMyOrg1() {
+    setGlobalsForPeer0Org1
+    # set -x
+    peer lifecycle chaincode approveformyorg -o localhost:7050 \
+        --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --version ${VERSION} --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION}
+    # set +x
+
+    echo "===================== chaincode approved from org 1 ===================== "
+
+}
+
+# approveForMyOrg1
+
+getBlock() {
+    setGlobalsForPeer0Org1
+    peer channel getinfo  -c ${CHANNEL_NAME} -o localhost:7050 
+}
+
+# getBlock
+
+checkCommitReadyness() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode checkcommitreadiness \
+        --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+        --sequence ${VERSION} --output json --init-required
+    echo "===================== checking commit readyness from org 1 ===================== "
+}
+commitChaincodeDefination() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode commit -o localhost:7050  \
+        --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --peerAddresses localhost:7051 \
+        --peerAddresses localhost:9051 \
+        --peerAddresses localhost:11051 \
+        --version ${VERSION} --sequence ${VERSION} \
+        --init-required
+}
+queryCommitted() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode querycommitted --channelID $CHANNEL_NAME --name ${CC_NAME}
+
+}
+
+
+
+packageChaincode
+installChaincode
+queryInstalled
+approveForMyOrg1
+checkCommitReadyness
+
